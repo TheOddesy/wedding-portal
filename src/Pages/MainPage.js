0a59b84662e53b7cea693ecba9d110d2f13658e2
Nicolas <fob@qlik.com>
2023-12-13 22:05:08 +0100

    add: tooltip to sidebar, sizing issues for page

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -8,11 +8,11 @@ import './LanguageToggler.css';
 
 const MainPage = () => {
   const {languageData} = useContext(LanguageContext);
+  const zero = useRef(null);
   const one = useRef(null);
   const two = useRef(null);
   const three = useRef(null);
   const four = useRef(null);
-  const five = useRef(null);
 
   const scrollToSection = (elementRef) => {
     window.scrollTo({
@@ -25,17 +25,17 @@ const MainPage = () => {
     <div className='mainPage'>
       <SideBar
         scrollToSection={scrollToSection}
+        zero={zero}
         one={one}
         two={two}
         three={three}
         four={four}
-        five={five}
       />
       <LanguageToggler />
       <div className='smallSite'>{languageData.smallsite}</div>
 
-      <div className='bigSite'>
-        <div ref={one} className='titleWrapper'>
+      <div ref={zero} className='bigSite'>
+        <div className='titleWrapper'>
           <div className='masqualero mainTitle'>{languageData.nicolas}</div>
           <div className='cormorant mainTitleAnd'>{languageData.and}</div>
           <div className='masqualero mainTitle'>{languageData.lovisa}</div>
@@ -43,26 +43,26 @@ const MainPage = () => {
 
         <div className='spaceForTesting'></div>
 
+        <div ref={one} className='cormorant secondaryTitle'>
+          {languageData.subtitle1}
+        </div>
+        <div className='sourceSans'>{languageData.text}</div>
+        <div className='spaceForTesting'></div>
+
         <div ref={two} className='cormorant secondaryTitle'>
-          {languageData.secondaryTitle}&nbsp;1
+          {languageData.subtitle2}
         </div>
         <div className='sourceSans'>{languageData.text}</div>
         <div className='spaceForTesting'></div>
 
         <div ref={three} className='cormorant secondaryTitle'>
-          {languageData.secondaryTitle}&nbsp;2
+          {languageData.subtitle3}
         </div>
         <div className='sourceSans'>{languageData.text}</div>
         <div className='spaceForTesting'></div>
 
         <div ref={four} className='cormorant secondaryTitle'>
-          {languageData.secondaryTitle}&nbsp;3
-        </div>
-        <div className='sourceSans'>{languageData.text}</div>
-        <div className='spaceForTesting'></div>
-
-        <div ref={five} className='cormorant secondaryTitle'>
-          {languageData.secondaryTitle}&nbsp;4
+          {languageData.subtitle4}
         </div>
         <div className='sourceSans'>{languageData.text}</div>
         <div className='spaceForTesting'></div>
--- src/Pages/SideBar.js
+++ src/Pages/SideBar.js
@@ -1,13 +1,27 @@
-const SideBar = ({scrollToSection, one, two, three, four, five}) => {
+import {useContext} from 'react';
+import {LanguageContext} from '../Store/LanguageContext';
+
+const SideBar = ({scrollToSection, zero, one, two, three, four}) => {
+  const {languageData} = useContext(LanguageContext);
   return (
     <div className='sideBar'>
       <div className='verticalLine'></div>
       <div className='menuItemsWrapper'>
-        <div onClick={() => scrollToSection(one)} className='menuItem'></div>
-        <div onClick={() => scrollToSection(two)} className='menuItem'></div>
-        <div onClick={() => scrollToSection(three)} className='menuItem'></div>
-        <div onClick={() => scrollToSection(four)} className='menuItem'></div>
-        <div onClick={() => scrollToSection(five)} className='menuItem'></div>
+        <div onClick={() => scrollToSection(zero)} className='menuItem'>
+          <div className='tooltip sourceSans'>{languageData.home}</div>
+        </div>
+        <div onClick={() => scrollToSection(one)} className='menuItem'>
+          <div className='tooltip sourceSans'>{languageData.subtitle1}</div>
+        </div>
+        <div onClick={() => scrollToSection(two)} className='menuItem'>
+          <div className='tooltip sourceSans'>{languageData.subtitle2}</div>
+        </div>
+        <div onClick={() => scrollToSection(three)} className='menuItem'>
+          <div className='tooltip sourceSans'>{languageData.subtitle3}</div>
+        </div>
+        <div onClick={() => scrollToSection(four)} className='menuItem'>
+          <div className='tooltip sourceSans'>{languageData.subtitle4}</div>
+        </div>
       </div>
       <div className='verticalLine'></div>
     </div>
