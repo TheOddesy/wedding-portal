0cdf87c97b27f6089032f223c3819ee07f48bcbd
Nicolas <fob@qlik.com>
2024-01-17 21:36:28 +0100

    fix: changed wording to be more concise

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Pages/MainPage';
+import MainPage from './Pages/MainPage';
 import {LanguageProvider} from './Store/LanguageContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <LanguageProvider>
     <React.StrictMode>
-      <App />
+      <MainPage />
     </React.StrictMode>
   </LanguageProvider>
 );
