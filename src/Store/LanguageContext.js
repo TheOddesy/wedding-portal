d07f8ce874524045172da869ab4660c57f9bfd87
Nicolas <fob@qlik.com>
2024-01-23 15:31:46 +0100

    fix: the language context so json files can be got

--- src/Store/LanguageContext.js
+++ src/Store/LanguageContext.js
@@ -12,7 +12,7 @@ const LanguageProvider = ({children}) => {
     fetchLanguageData();
   }, [language]);
   const fetchLanguageData = () => {
-    const dataUrl = `http://nicolaslovisa.wedding/Languages/${language}.json`;
+    const dataUrl = `http://nicolaslovisa.wedding/languages/${language}.json`;
     fetch(dataUrl)
       .then((response) => response.json())
       .then((data) => setLanguageData(data))
@@ -25,6 +25,6 @@ const LanguageProvider = ({children}) => {
   );
 };
 export {LanguageContext, LanguageProvider};
-// const dataUrl = `http://localhost:3000/wedding-portal/Languages/${language}.json`;
-// const dataUrl = `https://theoddesy.github.io/wedding-portal/Languages/${language}.json`;
-// const dataUrl = `http://www.nicolaslovisa.wedding/Languages/${language}.json`;
+// const dataUrl = `http://localhost:3000/languages/${language}.json`;
+// const dataUrl = `https://theoddesy.github.io/wedding-portal/languages/${language}.json`;
+// const dataUrl = `http://nicolaslovisa.wedding/languages/${language}.json`;
